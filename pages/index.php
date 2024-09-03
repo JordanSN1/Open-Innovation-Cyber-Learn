7e62a93da2b6c0821099f0499f539204abcb8b82
jordanSN1 <jordan.turnaco@ecoles-epsi.net>
2024-09-03 10:36:59 +0200

    first

---
+++ pages/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CyberLearn - Home </title>
+</head>
+<body>
+    <nav class="Navbar">
+        <ul class="Sidebar">
+            <li class="list-image"><a href="index.html"><img class="HomeImage" src="../Images/Log_pricipal_large.png" alt="HomeImage"></a></li>
+
+            <li class="Nav-List"><a class="links" href="Pages/lecons.php">Nos Leçons</a></li> <br>
+            <li class="Nav-List"><a class="links" href="Pages/apropos.php">A propos</a></li> <br>
+            <li class="Nav-List"><a class="links" href="Pages/contact.php">Support</a></li> <br>
+        </ul>
+    </nav>
+    
+</body>
+</html>
